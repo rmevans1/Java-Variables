91df89e1e7fe70b40e408be46cc78c742dd3478a
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:47:47 -0400

    Append to a string

--- src/Lecture20.java
+++ src/Lecture20.java
@@ -11,5 +11,9 @@ public class Lecture20 {
 
         String myString = "This is a string";
         System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2015";
+        System.out.println("myString is equal to " + myString);
     }
 }
