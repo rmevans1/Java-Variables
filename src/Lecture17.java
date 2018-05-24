34f09d136547ec62e38efa31f4fb48fd48ad11b6
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 15:46:13 -0400

    Add byte

--- src/Lecture17.java
+++ src/Lecture17.java
@@ -2,5 +2,7 @@ public class Lecture17 {
     public static void main(String[] args) {
         int myMinValue = -2_147_483_648; //min integer
         int myMaxValue = 2_147_483_647;  //max integer
+
+        byte myByteValue = -128; //-128 - 127
     }
 }
