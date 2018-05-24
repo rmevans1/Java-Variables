2b8002b9fb8bdc8bcd0ec17dc51bfa89f3f56345
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:58:20 -0400

    =,- operator

--- src/Lecture21.java
+++ src/Lecture21.java
@@ -2,5 +2,9 @@ public class Lecture21 {
     public static void main(String[] args) {
         int result = 1 + 2; //operators =,+
         System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+        result = result - 1; //operators =,-
+        System.out.println(previousResult + " - 1 = " + result);
     }
 }
