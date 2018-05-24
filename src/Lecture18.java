3f385f1092ac76ed54915bd981960c1d28bbf7d2
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:16:59 -0400

    Display int, float, and double values

--- src/Lecture18.java
+++ src/Lecture18.java
@@ -3,5 +3,8 @@ public class Lecture18 {
         int myIntValue = 5;
         float myFloatValue = 5f;
         double myDoubleValue = 5d;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
     }
 }
