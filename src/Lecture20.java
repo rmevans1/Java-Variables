e017ff53822ee7f9e7f0d67777e71cabfd253108
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:46:08 -0400

    Display a string

--- src/Lecture20.java
+++ src/Lecture20.java
@@ -1,5 +1,15 @@
 public class Lecture20 {
     public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
 
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
     }
 }
