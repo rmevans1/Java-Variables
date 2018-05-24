18232e9060aecc1d0a0b75f908dc8bf9e4cdbf63
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:39:10 -0400

    Complete challenge

--- src/Lecture19.java
+++ src/Lecture19.java
@@ -8,5 +8,12 @@ public class Lecture19 {
         //used for testing
         boolean myBoolean = false;
         boolean isMale = true;
+
+        //Challenge
+        // 1. Find the code for the registered symbol on the same line as the copyright symbol.
+        // 2. Create a variable of type char and assign it the unicode value for that symbol.
+        // 3. Display it on screen.
+        char registered = '\u00AE';
+        System.out.println("Registered Symbol: " + registered);
     }
 }
