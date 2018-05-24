0e203b57b713e2dd5c1ea5265239105dd49a0ff5
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 15:43:30 -0400

    Add underscores to make numbers more readable

--- src/Lecture17.java
+++ src/Lecture17.java
@@ -1,6 +1,6 @@
 public class Lecture17 {
     public static void main(String[] args) {
-        int myMinValue = -2147483648; //min integer
-        int myMaxValue = 2147483647;  //max integer
+        int myMinValue = -2_147_483_648; //min integer
+        int myMaxValue = 2_147_483_647;  //max integer
     }
 }
