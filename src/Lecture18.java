b102934dac209c8a06c875e17e64932dfa646206
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:19:42 -0400

    Divide by 2

--- src/Lecture18.java
+++ src/Lecture18.java
@@ -6,5 +6,13 @@ public class Lecture18 {
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("myDoubleValue = " + myDoubleValue);
+
+        //Divide by 2
+        int myIntValue2 = 5 / 2;
+        float myFloatValue2 = 5f / 2f;
+        double myDoubleValue2 = 5d / 2d;
+        System.out.println("myIntValue2 = " + myIntValue2);
+        System.out.println("myFloatValue2 = " + myFloatValue2);
+        System.out.println("myDoubleValue2 = " + myDoubleValue2);
     }
 }
