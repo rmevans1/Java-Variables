fb76aaba9ca1f01c7fe6ae5cdc104d743c34f1c8
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 15:48:39 -0400

    Add data type widths

--- src/Lecture17.java
+++ src/Lecture17.java
@@ -1,10 +1,14 @@
 public class Lecture17 {
     public static void main(String[] args) {
+
+        //int has a width of 32
         int myMinValue = -2_147_483_648; //min integer
         int myMaxValue = 2_147_483_647;  //max integer
 
+        //byte has a width of 8
         byte myByteValue = -128; //-128 - 127
 
+        //byte has a width of 16
         short myShortValue = 30000; // -32768 - 32767
     }
 }
