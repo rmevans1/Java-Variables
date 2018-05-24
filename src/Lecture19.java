b0377eb9a47bf80ac5f055dfd6784ae289f08695
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 16:36:32 -0400

    unicode characters

--- src/Lecture19.java
+++ src/Lecture19.java
@@ -1,5 +1,7 @@
 public class Lecture19 {
     public static void main(String[] args){
-
+        char myChar = '$'; //can ony be 1 character
+        char myChar2 = '\u00A9'; //copyright unicode
+        System.out.println("Unicode output was: " + myChar2);
     }
 }
