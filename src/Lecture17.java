d3e7b6c65a2d967f519239649a27ddc08e17530c
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 15:42:13 -0400

    Min and max integer

--- src/Lecture17.java
+++ src/Lecture17.java
@@ -1,2 +1,6 @@
 public class Lecture17 {
+    public static void main(String[] args) {
+        int myMinValue = -2147483648; //min integer
+        int myMaxValue = 2147483647;  //max integer
+    }
 }
