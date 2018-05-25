2fa402e14be7d2dfe7110d99f3f67699adda5ec6
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 20:52:26 -0400

    OR operator

--- src/Lecture22.java
+++ src/Lecture22.java
@@ -1,5 +1,10 @@
 public class Lecture22 {
     public static void main(String[] args){
+        int topScore = 80;
+        int secondTopScore = 81;
 
+        //OR Operator
+        if((topScore >90) || (secondTopScore <=90))
+            System.out.println("One of these tests is true");
     }
 }
