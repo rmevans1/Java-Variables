569b38692737916299897c1621f049cc0505ab8b
Robert Evans <robertevans@robertevansmb.com>
2018-05-24 21:10:11 -0400

    Completed challenge

--- src/Lecture22.java
+++ src/Lecture22.java
@@ -23,5 +23,20 @@ public class Lecture22 {
         boolean wasCar = isCar ? true : false; //if is car == true wasCar = true else wasCar = false
         if(wasCar)
             System.out.println("wasCar is true");
+
+        // 1. Create a double variable with the value 20
+        // 2. Create a second variable of type double with the value 80
+        // 3. Add both numbers up and multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that displays a message "Total was over the limit"
+        //    if the remaining total (#4) is equal to 20 or less.
+        double myDouble = 20;
+        double mySecondDouble = 80;
+        double total = (myDouble + mySecondDouble) * 25;
+        System.out.println("total = " + total);
+        double remainder = total % 40;
+        System.out.println("Remainder is " + remainder);
+        if(remainder <= 20)
+            System.out.println("Total was over the limit");
     }
 }
